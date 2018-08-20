11dd8227301520966b5a728205088a44a071f324
lavanyachitra <lavanya19399@gmail.com>
2018-08-20 13:44:11 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Vowel {
+
+public static void main(String[] args) {
+
+char c= 'i';
+
+if(c== 'a' || c == 'e' || c== 'i' || c == 'o' || c== 'u' )
+{
+ System.out.println(ch + " is vowel");
+}
+ else
+{
+ System.out.println(ch + " is consonant");
+}
+}
+}
