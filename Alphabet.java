74ce7e6aee886750c40182386840e6f190ec4a98
lavanyachitra <lavanya19399@gmail.com>
2018-08-20 14:06:10 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class Alpahbet
+{
+public static void main(String[] args)
+{
+char d='A';
+if(d>='a' &&d<='z')||(d>='A' && d>='Z')
+{
+System.out.println(d +"is alphabet");
+}
+else
+{
+System.out.println(d +"is NOT alphabet");
+}
+}
+}
